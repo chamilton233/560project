967b13a03494d30f3389a2f86c6807130e00aecd
Mike Ruckert <mwruckert@gmail.com>
2020-05-03 21:49:33 -0500

    added ui

--- Database/FrontEnd/Properties/Resources.Designer.cs
+++ Database/FrontEnd/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace _560FrontEnd.Properties
+namespace FrontEnd.Properties
 {
 
 
@@ -44,7 +44,7 @@ namespace _560FrontEnd.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("_560FrontEnd.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("FrontEnd.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- Database/FrontEnd/Properties/Settings.Designer.cs
+++ Database/FrontEnd/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace _560FrontEnd.Properties
+namespace FrontEnd.Properties
 {
 
 
